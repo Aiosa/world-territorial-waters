25c132f1234531525dab1f0ea81bc6f2c8f14ada
Aiosa <horakj7@gmail.com>
2020-12-30 14:52:39 +0100

    change port

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const PORT = process.env.PORT || 3000
 
 app.use(express.static('public'))
 app.use('/public', express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => res.sendFile(__dirname + "/index.html"))
 
-app.listen(port, () => console.log(`Listening on port ${port}!`))
+app.listen(PORT, () => console.log(`Listening on port ${PORT}!`))
